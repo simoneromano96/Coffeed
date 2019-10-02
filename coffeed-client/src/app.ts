61dabcc3a29ce25fbeb700f125b7c3535d76444c
Simone Romano <simoneromano@protonmail.ch>
2019-10-02 22:34:37 +0200

    Added sanitize, some wip

--- coffeed-client/src/app.ts
+++ coffeed-client/src/app.ts
@@ -1,3 +1,14 @@
+import { RouterConfiguration, Router } from 'aurelia-router';
+import { PLATFORM } from "aurelia-framework"; //! Needed for webpack
+
 export class App {
-  public message: string = 'Hello World!';
+  router: Router;
+
+  configureRouter(config: RouterConfiguration, router: Router) {
+    this.router = router;
+    config.title = "Coffeed";
+    config.map([
+      { route: "", name: "homepage", moduleId: PLATFORM.moduleName("./components/homepage/homepage") }
+    ]);
+  }
 }
