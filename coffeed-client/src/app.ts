9bd473c52fbbaabab73c119b87b3e8c2e4413fb5
Simone Romano <simoneromano@protonmail.ch>
2019-10-03 22:51:00 +0200

    Routing and some images

--- coffeed-client/src/app.ts
+++ coffeed-client/src/app.ts
@@ -8,7 +8,9 @@ export class App {
     this.router = router;
     config.title = "Coffeed";
     config.map([
-      { route: "", name: "homepage", moduleId: PLATFORM.moduleName("./components/homepage/homepage") }
+      { route: "", name: "homepage", moduleId: PLATFORM.moduleName("./pages/homepage/homepage") },
+      { route: "about", name: "about", moduleId: PLATFORM.moduleName("./pages/about/about") },
+      { route: "shop", name: "shop", moduleId: PLATFORM.moduleName("./pages/shop/shop") },
     ]);
   }
 }
