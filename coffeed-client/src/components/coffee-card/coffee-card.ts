95a8497631e7f90e9c8c679dc99405d908a80d4a
Simone Romano <simoneromano@protonmail.ch>
2019-10-08 21:08:23 +0200

    WIP, last AureliaJS commit

--- coffeed-client/src/components/coffee-card/coffee-card.ts
+++ coffeed-client/src/components/coffee-card/coffee-card.ts
@@ -1,13 +1,16 @@
-import { bindable } from 'aurelia-framework';
-import { Coffee } from "classes/coffee";
+import { bindable } from "aurelia-framework"
+import { Coffee } from "classes/coffee"
 
 export class CoffeeCard {
-  @bindable coffee: Coffee;
-  loading: boolean;  
+  @bindable coffee: Coffee
+  // loading: boolean
+  loadingPercentage: number = 0
 
   fetchDetails = () => {
-    this.loading = true;
-    this.coffee.fetchDetails();
-    this.loading = false;
+    this.loadingPercentage = 0
+    this.coffee.fetchDetails()
+    this.loadingPercentage = 100
   }
+
+  getName = () => this.coffee.name
 }
